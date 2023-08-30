9da2df99fcc86d807c9df8a3238877d43b49f305
Nadxelle Hernandez <nadxelleHernandez@gmail.com>
2023-08-30 15:59:52 -0700

    Finished calls to api for Top Shows and Movies. HomePage Displaying properly Changed some styles to media-card

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button'
 import { TopMediaListComponent } from '../components/top-media-list/top-media-list.component';
+import { Media } from '../interfaces/media';
+import { ReviewgramAPIService } from '../reviewgram-api.service';
 
 
 @Component({
@@ -15,5 +17,16 @@ import { TopMediaListComponent } from '../components/top-media-list/top-media-li
 })
 
 export class HomeComponent {
-  
+  topMoviesList: Media[] = [];
+  topTVShowsList: Media[] = [];
+  apiService: ReviewgramAPIService = inject(ReviewgramAPIService);
+
+  constructor() {
+    this.apiService.getTopMoviesFromAPI().then((topMovies: Media[]) => {
+      this.topMoviesList = topMovies;
+    });
+    this.apiService.getTopTVShowsFromAPI().then((topShows) => {
+      this.topTVShowsList = topShows;
+    });
+  }
 }
--- src/app/reviewgram-api.service.ts
+++ src/app/reviewgram-api.service.ts
@@ -7,13 +7,15 @@ import { Media } from './interfaces/media';
 
 export class ReviewgramAPIService {
   baseURL = 'http://127.0.0.1:5000'
+  imagesURL = '';
 
-  constructor() { }
+  constructor() {
+   }
 
-  async getImagesUrlFromAPI(): Promise<String>  {
+  async getImagesUrlFromAPI(): Promise<string>  {
     const response = await fetch(`${this.baseURL}/media/image-url`, {method: 'GET'});
 
-    let imageUrl = ''
+    let imageUrl = '';
     if (response.ok) {
       const data = await response.json() ?? {};
       imageUrl = data.configuration.base_url;
@@ -31,18 +33,41 @@ export class ReviewgramAPIService {
     let movies = [];
     if(response.ok) {
       const data = await response.json() ?? {};
-      const imageUrl = await this.getImagesUrlFromAPI();
       movies = data.movies;
+      if(this.imagesURL==='') {
+        this.imagesURL = await this.getImagesUrlFromAPI();
+      }
       for (let movie of movies) {
-        if(movie.poster_url) {
-          movie.poster_url = `${imageUrl}w154${movie.poster_url}`;
+        let newPosterUrl = `${this.imagesURL}w154${movie.poster_url}`;
+        movie.poster_url = newPosterUrl;
+      }
+    } else {
+      //Show error component
+      this.logError('Getting Top Movies failed', response);
+    }
+    return movies;
+  }
+
+  async getTopTVShowsFromAPI(): Promise<Media[]>{
+    const response = await fetch(`${this.baseURL}/media/top/tvshows`, {method: 'GET'});
+
+    let tvshows = [];
+    if(response.ok) {
+      const data = await response.json() ?? {};
+      tvshows = data.tvshows;
+      if(this.imagesURL==='') {
+        this.imagesURL = await this.getImagesUrlFromAPI();
+      }
+      for (let show of tvshows) {
+        if(show.poster_url) {
+          show.poster_url = `${this.imagesURL}w154${show.poster_url}`;
         }
       }
     } else {
       //Show error component
-      this.logError('Getting ImagesURL failed', response);
+      this.logError('Getting Top TV Shows failed', response);
     }
-    return movies;
+    return tvshows;
   }
 
   logError(message: string, response: Response) {
